c61b234c3753a767b974a4ead25a66a09c7bb976
YKou-Kody <83717117+YKou-Kody@users.noreply.github.com>
2022-02-05 13:56:47 -0700

    added components folder

---
+++ react/src/router.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+
+
+
+const router = () => {
+    return <div></div>;
+};
+
+export default router;
