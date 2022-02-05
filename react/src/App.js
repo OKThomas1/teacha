9edf8908d6d4830a72f13f218aa0eaa47a7d885c
YKou-Kody <83717117+YKou-Kody@users.noreply.github.com>
2022-02-05 15:00:47 -0700

    added one route for match

--- react/src/App.js
+++ react/src/App.js
@@ -1,14 +1,16 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from './Components/Home/Home';
 import { Edit } from './Components/Edit-profile/Edit';
+import { Match } from "./Components/Match/Match";
 
 function App() {
   return (
-    <div className='App'>
+    <div className='App w-100'>
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home/>}/>
-          <Route path="/edit-profile" element={<Edit/>}/>
+          <Route path="/home" element={<Home />} />
+          <Route path="/edit-profile" element={<Edit />} />
+          <Route path="/match" element={<Match />} />
         </Routes>
       </BrowserRouter>
     </div>
