03497d43542db6aedd0e7d93dbe62346cd2274d2
YKou-Kody <83717117+YKou-Kody@users.noreply.github.com>
2022-02-05 20:04:47 -0700

    fixed typo

--- react/src/App.js
+++ react/src/App.js
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
+
 
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Home } from "./Components/Home/Home"
 import { Edit } from "./Components/Edit-profile/Edit"
 import { Settings } from "./Components/Settings/Settings"
 import { Match } from "./Components/Match/Match"
-=======
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import {Home} from "./Components/Home/Home"
-import {Edit} from "./Components/Edit-profile/Edit"
-import {Settings} from "./Components/Settings/Settings"
 
->>>>>>> 6beaa2fdb8bb811835e82e0933a199074bf1de28
 function App() {
   return (
     <div className="App">
