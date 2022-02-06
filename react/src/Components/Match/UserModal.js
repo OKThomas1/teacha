9a5a7e6f450f348be9625cf0baff48cac3b95e50
Rohinesh Ram <rohinesh@ualberta.ca>
2022-02-06 04:59:37 -0700

    Added props to usermodal

--- react/src/Components/Match/UserModal.js
+++ react/src/Components/Match/UserModal.js
@@ -3,7 +3,6 @@ import React from 'react';
 
 const UserModal = (props) => {
 
-  let name = "Roh"
 
   const imageStyle = {
     borderRadius:'50%',
@@ -24,41 +23,41 @@ const UserModal = (props) => {
         className="card bg-light d-flex" 
         style={{ height: "90%", width: "50%", position: "absolute", left:"25%", top:"10%", zIndex: "5", overflow: "auto" }}>
         <div class="card-header " style={{ backgroundColor: "#FFF4E0" }}>
-            <h1>{name}</h1>
+            <h1>{props.first_name + " " + props.last_name}</h1>
         </div>
         <div className="card-body d-flex flex-column align-items-center" style={{ backgroundColor: "#FFF4E0" }} >
           <img 
-            src="https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg" 
+            src={props.avatar} 
             alt=""
             style={imageStyle}
           />
           <div class="form-row pt-4">
             <div class="form-group col-md-6">
               <label>Work</label>
-              <input type="text" class="form-control" placeholder="Work" readOnly/>
+              <input type="text" class="form-control" placeholder={props.work} readOnly/>
             </div>
             <div class="form-group col-md-6">
               <label>Job Title</label>
-              <input type="text" class="form-control" placeholder="JOB" readOnly/>
+              <input type="text" class="form-control" placeholder={props.job_title} readOnly/>
             </div>
           </div>
           <div class="form-row">
             <div class="form-group col-md-6">
               <label>School</label>
-              <input type="text" class="form-control" placeholder="SCHOOL" readOnly/>
+              <input type="text" class="form-control" placeholder={props.school} readOnly/>
             </div>
             <div class="form-group col-md-6">
               <label>Education Level</label>
-              <input type="text" class="form-control" placeholder="EDUCATION" readOnly/>
+              <input type="text" class="form-control" placeholder={props.education_level} readOnly/>
             </div>
           </div>
           <div class="form-row">
               <label>Home Town</label>
-              <input type="text" class="form-control" placeholder="HOME TOWN" readOnly/>
+              <input type="text" class="form-control" placeholder={props.hometown} readOnly/>
           </div>
           <div class="form-row" style={{width:"80%", height:"25%"}}>
               <label>Bio</label>
-              <input type="text" class="form-control" placeholder="BIO" readOnly/>
+              <input type="text" class="form-control" placeholder={props.bio} readOnly/>
           </div>
         </div>
       </div>
